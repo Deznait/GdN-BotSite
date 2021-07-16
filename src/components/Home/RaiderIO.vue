2a7f744678c6fd643d8aa0394c615167a391843d
Alvaro Olea Rojas <alvaroolear@gmail.com>
2021-07-17 00:00:23 +0200

    RaiderIO saved on LocalStorage

--- src/components/Home/RaiderIO.vue
+++ src/components/Home/RaiderIO.vue
@@ -42,9 +42,15 @@
                             </div>
                             <q-linear-progress
                                 class="Progressbar"
+                                :class="
+                                    'progresslevel_' + difficulty.progress_level
+                                "
                                 size="30px"
-                                :value="difficulty.bosses_killed / raid.prog.total_bosses"
-                                track-color="dark"
+                                :value="
+                                    difficulty.bosses_killed /
+                                    raid.prog.total_bosses
+                                "
+                                track-color="red"
                             >
                                 <q-badge
                                     class="
@@ -67,7 +73,12 @@
 
                 <div class="q-pb-md q-px-md text-subtitle2 text-center">
                     <a :href="guildInfo.url" target="_blank">
-                        <q-btn size="sm" flat color="primary" label="Más info en RaiderIO" />
+                        <q-btn
+                            size="sm"
+                            flat
+                            color="primary"
+                            label="Más info en RaiderIO"
+                        />
                     </a>
                 </div>
             </q-card>
@@ -78,6 +89,7 @@
 <script>
 import axios from "axios";
 import { ref } from "vue";
+import { useQuasar } from "quasar";
 
 const realmNames = {
     "castle-nathria": "Castillo de Nathria",
@@ -93,30 +105,41 @@ export default {
         };
     },
     setup() {
+        
+
         return {
             raidTab: ref("sanctum-of-domination"),
         };
     },
     mounted() {
-        // Send a POST request
-        axios({
-            method: "get",
-            url: "https://raider.io/api/v1/guilds/profile",
-            params: {
-                region: "eu",
-                realm: "sanguino",
-                name: "gremio de nordrassil",
-                fields: "raid_progression,raid_rankings",
-            },
-        }).then(
-            (response) => {
-                this.processInfo(response);
-            },
-            (error) => {
-                console.log("error");
-                console.log(error);
-            }
-        );
+        const $q = useQuasar();
+        const savedGuildInfo = $q.localStorage.getItem("savedGuildInfo");
+
+        if(savedGuildInfo === null){
+            // Send a POST request
+            axios({
+                method: "get",
+                url: "https://raider.io/api/v1/guilds/profile",
+                params: {
+                    region: "eu",
+                    realm: "sanguino",
+                    name: "gremio de nordrassil",
+                    fields: "raid_progression,raid_rankings",
+                },
+            }).then(
+                (response) => {
+                    let guild_info = this.processInfo(response);
+                    $q.localStorage.set("savedGuildInfo", guild_info)
+                    this.guildInfo = guild_info;
+                },
+                (error) => {
+                    console.log("error");
+                    console.log(error);
+                }
+            );
+        }else {
+            this.guildInfo = savedGuildInfo;
+        }
     },
     methods: {
         processInfo(response) {
@@ -138,14 +161,26 @@ export default {
                         normal: {
                             name: "Normal",
                             bosses_killed: raid_prog.normal_bosses_killed,
+                            progress_level: this.progressionLevel(
+                                raid_prog.normal_bosses_killed,
+                                raid_prog.total_bosses
+                            ),
                         },
                         heroic: {
                             name: "Heroico",
                             bosses_killed: raid_prog.heroic_bosses_killed,
+                            progress_level: this.progressionLevel(
+                                raid_prog.heroic_bosses_killed,
+                                raid_prog.total_bosses
+                            ),
                         },
                         mythic: {
                             name: "Mítico",
                             bosses_killed: raid_prog.mythic_bosses_killed,
+                            progress_level: this.progressionLevel(
+                                raid_prog.mythic_bosses_killed,
+                                raid_prog.total_bosses
+                            ),
                         },
                     },
                 };
@@ -167,13 +202,27 @@ export default {
                 });
             }
 
-            this.guildInfo = guild_info;
+            return guild_info;
+        },
+        progressionLevel(bosses_killed, total_bosses) {
+            let progress_level;
+            let progressRatio = bosses_killed / total_bosses;
+
+            if (progressRatio >= 0.66) {
+                progress_level = "high";
+            } else if (progressRatio >= 0.33) {
+                progress_level = "medium";
+            } else {
+                progress_level = "low";
+            }
+
+            return progress_level;
         },
     },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .RaidProgress-difficulties {
     line-height: 1.5;
     text-transform: uppercase;
@@ -187,8 +236,57 @@ export default {
         border: 1px solid hsla(0, 0%, 100%, 0.2);
         position: relative;
 
+        .q-linear-progress__track {
+            background: #181818 !important;
+        }
+        .q-linear-progress__model {
+            color: transparent;
+        }
+
         .Progressbar-fraction {
             text-shadow: 0 0 1px transparent, 0 1px 2px rgba(0, 0, 0, 0.8);
+        }
+
+        &.progresslevel_low {
+            .q-linear-progress__model {
+                background: #99755c;
+                background: -webkit-gradient(
+                    linear,
+                    left top,
+                    right top,
+                    from(#69503f),
+                    to(#99755c)
+                );
+                background: linear-gradient(90deg, #69503f 0, #99755c);
+            }
+        }
+
+        &.progresslevel_medium {
+            .q-linear-progress__model {
+                background: #c76700;
+                background: -webkit-gradient(
+                    linear,
+                    left top,
+                    right top,
+                    from(#7b3f00),
+                    to(#c76700)
+                );
+                background: linear-gradient(90deg, #7b3f00 0, #c76700);
+            }
+        }
+
+        &.progresslevel_high {
+            .q-linear-progress__model {
+                background: #1b9601;
+                background: -webkit-gradient(
+                    linear,
+                    left top,
+                    right top,
+                    from(#0d4a00),
+                    to(#1b9601)
+                );
+                background: linear-gradient(90deg, #0d4a00 0, #1b9601);
+            }
         }
     }
 }
