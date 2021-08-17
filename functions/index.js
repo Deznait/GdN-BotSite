5dea8b70c7a3cb74341f88955632e5fda98d8059
Alvaro Olea Rojas <alvaroolear@gmail.com>
2021-08-17 19:42:51 +0200

    Scheduler

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,10 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.scheduledFunction = functions.pubsub.schedule("5 11 * * *")
+    .timeZone("Europe/Madrid").onRun((context) => {
+      functions.logger.info("Hello logs!", {structuredData: true});
+      console.info("This will be run every day at 11:05 AM!");
+      return null;
+    });
