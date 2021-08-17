54bdbfaf700516e7f6d3354fd6c5de3fc2b7a2dd
Alvaro Olea Rojas <alvaroolear@gmail.com>
2021-08-17 20:53:29 +0200

    New Functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,27 @@
 const functions = require("firebase-functions");
+const axios = require("axios").default;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+
+  axios.get("https://www.wowprogress.com/guild/eu/sanguino/Gremio+de+Nordrassil/json_rank/")
+      .then(function(response) {
+        console.log(response.data);
+        functions.logger
+            .info("WowProgress_Update", {wp_update: response.data});
+      });
 });
 
-exports.scheduledFunction = functions.pubsub.schedule("5 11 * * *")
+exports.scheduledFunction = functions.pubsub.schedule("30 2 * * *")
     .timeZone("Europe/Madrid").onRun((context) => {
-      functions.logger.info("Hello logs!", {structuredData: true});
-      console.info("This will be run every day at 11:05 AM!");
+      console.info("This will be run every day at 2:30 AM!");
+
+      axios.get("https://www.wowprogress.com/guild/eu/sanguino/Gremio+de+Nordrassil/json_rank/")
+          .then(function(response) {
+            console.log(response.data);
+            functions.logger
+                .info("WowProgress_Update", {wp_update: response.data});
+          });
+
       return null;
     });
