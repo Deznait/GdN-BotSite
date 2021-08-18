52978ccc5bef7e763fd91f00b50d5a56f6eabbfb
Alvaro <alvaroolear@gmail.com>
2021-08-19 01:17:08 +0200

    Update scripts

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const axios = require("axios").default;
-const admin = require("firebase-admin");
-admin.initializeApp();
+
+// const express = require("express");
+
+admin.initializeApp(functions.config().firebase);
 
 exports.testUpdate = functions
     .region("europe-west1")
