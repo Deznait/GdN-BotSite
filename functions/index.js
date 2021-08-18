b549130b907dd1fd16d1182b2a293d6738b7cec7
Alvaro <alvaroolear@gmail.com>
2021-08-18 18:14:41 +0200

    WoWprogress Update function

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,45 @@
 const functions = require("firebase-functions");
 const axios = require("axios").default;
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 exports.testUpdate = functions
     .region("europe-west1")
     .https
     .onRequest((request, response) => {
       response.send("Hello from Firebase!");
+      const db = admin.firestore();
 
       axios.get("https://www.wowprogress.com/guild/eu/sanguino/Gremio+de+Nordrassil/json_rank/")
           .then(function(response) {
-            console.log(response.data);
-            functions.logger
-                .info("WowProgress_Update_Request", {wp_update: response.data});
+            db.collection("wowprogress").doc("data").set(response.data)
+                .then(() => {
+                  functions.logger
+                      .info("WowProgress_Update - OK", {wp_update: response.data});
+                }).catch((error) => {
+                  functions.logger
+                      .error("WowProgress_Update - KO", {error: error});
+                });
           });
     });
 
-exports.scheduledFunction = functions
+exports.updateWowProgress = functions
     .region("europe-west1")
     .pubsub.schedule("30 2 * * *")
     .timeZone("Europe/Madrid")
-    .onRun((context) => {
-      console.info("This will be run every day at 2:30 AM!");
+    .onRun(() => {
+      const db = admin.firestore();
 
       axios.get("https://www.wowprogress.com/guild/eu/sanguino/Gremio+de+Nordrassil/json_rank/")
           .then(function(response) {
-            console.log(response.data);
-            functions.logger
-                .info("WowProgress_Update_Schedul", {wp_update: response.data});
+            db.collection("wowprogress").doc("data").set(response.data)
+                .then(() => {
+                  functions.logger
+                      .info("WowProgress_Update - OK", {wp_update: response.data});
+                }).catch((error) => {
+                  functions.logger
+                      .error("WowProgress_Update - KO", {error: error});
+                });
           });
 
       return null;
