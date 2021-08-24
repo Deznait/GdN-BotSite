e5b91237c30e9c9a25b731b697ca0c5384f31b78
Alvaro <alvaroolear@gmail.com>
2021-08-24 15:07:53 +0200

    Firebase Bug Fix

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,3 @@ exports.scheduled = require("./scheduled/index");
 
 // Express API functions
 exports.api = require("./api/index");
-
-
-const functions = require("firebase-functions");
-
-exports.newApi = functions.region("europe-west1").https.onRequest((req, res) => {
-  return res.status(200).json({message: "Hello World!"});
-});
--- functions/scheduled/index.js
+++ functions/scheduled/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 const admin = require("firebase-admin");
 const db = admin.firestore();
-const FieldValue = db.FieldValue;
+const FieldValue = admin.firestore.FieldValue;
 
 const blizzard = require("blizzard.js");
 const axios = require("axios").default;
