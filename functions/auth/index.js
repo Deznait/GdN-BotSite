01c56315b2354446668968ac38af994893476a83
Deznait <alvaroolear@gmail.com>
2021-09-03 01:21:24 +0200

    Account Creation with BattleTag

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -6,9 +6,11 @@ const crypto = require("crypto");
 // Firebase Setup
 const admin = require("firebase-admin");
 const db = admin.firestore();
-// const blizzard = require("blizzard.js");
+const blizzard = require("blizzard.js");
+const axios = require("axios").default;
 const cors = require("cors")({origin: true});
 
+const OAUTH_HOST = "https://eu.battle.net";
 const OAUTH_REDIRECT_URI = `https://${process.env.GCLOUD_PROJECT}.web.app/popup`;
 const OAUTH_SCOPES = "wow.profile";
 
@@ -25,7 +27,7 @@ function battleNetOAuth2Client() {
       secret: functions.config().battlenet.secret,
     },
     auth: {
-      tokenHost: "https://eu.battle.net",
+      tokenHost: OAUTH_HOST,
     },
   };
 
@@ -68,10 +70,11 @@ exports.battlenetToken = functions.region("europe-west1").https.onRequest(async 
       functions.logger.log("Auth code exchange result received:", results);
 
       // We have a BattleNet access token and the user identity now.
+      const accessToken = results.token.access_token;
       const battleNetUserID = results.token.sub;
 
       // Create a Firebase account and get the Custom Auth Token.
-      const firebaseToken = await createFirebaseAccount(battleNetUserID);
+      const firebaseToken = await createFirebaseAccount(accessToken, battleNetUserID);
 
       return res.jsonp({token: firebaseToken});
     } catch (error) {
@@ -84,35 +87,36 @@ exports.battlenetToken = functions.region("europe-west1").https.onRequest(async 
  * Creates a Firebase account with the given user profile and returns a custom auth token allowing
  * signing-in this account.
  *
+ * @param {String} accessToken
  * @param {String} battleNetUserID
  *
  * @return {Promise<string>} The Firebase custom auth token in a promise.
  */
-async function createFirebaseAccount(battleNetUserID) {
+async function createFirebaseAccount(accessToken, battleNetUserID) {
   // The UID we'll assign to the user.
   const uid = `battlenet:${battleNetUserID}`;
 
   // TODO Recover the user info from Blizz
-  const battletag = "XXXXX#1234";
-  const photoURL = "https://render-us.worldofwarcraft.com/character/tichondrius/00/000000000-avatar.jpg?alt=/shadow/avatar/2-1.jpg";
+  const photoURL = "https://render-us.worldofwarcraft.com/character/tichondrius/00/000000000-avatar.jpg?alt=/shadow/avatar/10-0.jpg";
+  const accountInfo = await getBattlenetAccountInfo(accessToken);
 
   // Save the access token to the Firestore Database.
   const databaseTask = db.collection("users").doc(uid).set({
     uid: uid,
-    characters: {},
-    battletag: battletag,
+    characters: accountInfo.characters,
+    battletag: accountInfo.battletag,
   });
 
   // Create or update the user account.
   const userCreationTask = admin.auth().updateUser(uid, {
-    displayName: battletag,
+    displayName: accountInfo.battletag,
     photoURL: photoURL,
   }).catch((error) => {
     // If user does not exists we create it.
     if (error.code === "auth/user-not-found") {
       return admin.auth().createUser({
         uid: uid,
-        displayName: battletag,
+        displayName: accountInfo.battletag,
         photoURL: photoURL,
       });
     }
@@ -124,12 +128,54 @@ async function createFirebaseAccount(battleNetUserID) {
   await Promise.all([userCreationTask, databaseTask]);
   // Create a Firebase custom auth token.
   const token = await admin.auth().createCustomToken(uid);
-  functions.logger.log(
-      "Created Custom token for UID \"",
-      uid,
-      "\" Token:",
-      token,
-  );
 
   return token;
 }
+
+/**
+ *
+ * @param {String} accessToken
+ *
+ * @return {Promise<string>}
+ */
+async function getBattlenetAccountInfo(accessToken) {
+  const userinfo = await await axios.get(`${OAUTH_HOST}/oauth/userinfo`, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+    },
+  });
+
+  const wowClient = await blizzard.wow.createInstance({
+    key: functions.config().battlenet.id,
+    secret: functions.config().battlenet.secret,
+    origin: "eu",
+    locale: "es_ES",
+    token: accessToken,
+  });
+
+  const accountProfile = await wowClient.accountProfile();
+
+  // Wait for all async tasks to complete, then generate and return a custom auth token.
+  await Promise.all([userinfo, accountProfile]);
+
+  let accountCharacters = [];
+
+  accountProfile.data.wow_accounts.forEach((wowAccount) => {
+    accountCharacters = accountCharacters.concat(wowAccount.characters);
+  });
+
+  const accountInfo = {
+    battletag: userinfo.data.battletag,
+    characters: accountCharacters,
+  };
+
+  return accountInfo;
+}
+
+
+exports.testing = functions.region("europe-west1").https.onRequest(async (req, res) => {
+  const accessToken = "EUmNvDD0cm6wBmL1QH12Jga74AaXYMZMT3";
+  const battleNetUserID = 924502;
+  const accountProfile = await createFirebaseAccount(accessToken, battleNetUserID);
+  functions.logger.log("accountProfile (testing)", accountProfile);
+});
